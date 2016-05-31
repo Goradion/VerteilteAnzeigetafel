37f061be09bfd20d7ba3e024bb43dd939d729f5d
Simon Bastian <SimonFBastian@googlemail.com>
2016-05-31 12:28:54 +0200

    a ist eine tolle variable

--- VerteilteSysteme/src/verteiltesysteme/VerteilteSysteme.java
+++ VerteilteSysteme/src/verteiltesysteme/VerteilteSysteme.java
@@ -16,7 +16,7 @@ public class VerteilteSysteme {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int a;
+        int a; //tolle variable
     }
     
 }
