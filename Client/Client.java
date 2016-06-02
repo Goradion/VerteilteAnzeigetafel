9b84eb590c5640fa4b713fcd954da8c87ae19b92
micha <micha@micha-VirtualBox>
2016-06-02 15:47:11 +0200

    Clientklasse hinzugefügt

---
+++ Client/Client.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Client;
+
+/**
+ *
+ * @author micha
+ */
+public class Client {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a; //tolle variable
+        System.out.println("hi");
+    }
+}
