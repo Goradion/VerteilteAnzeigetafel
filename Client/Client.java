c66b942475c23db6cc8662a706524523c15c73c4
micha <micha@micha-VirtualBox>
2016-06-03 10:23:31 +0200

    Get Methoden erweitert + kleinigkeiten GUI angelegt (nicht programmiert ) nur graphische oberfläche

--- Client/Client.java
+++ Client/Client.java
@@ -16,29 +16,42 @@ import java.net.Socket;
 public class Client {
     
     public static final int ENDE =0;
-    private String benutzername;
-    private int abtNr;
+    public static final int TRUE = 1;
+    public static final int FALSE = 0;
+    
+    private String benutzerName;
+    private int userID;
+    private int abtNr;//Abteilungsnummer
     private String message;
-    private int port=50000;
+    private int messageID;
+    static private int port = 50000; //festgelegter Port(frei)
     private String ip;
     private static Socket socket;
+    private boolean administrator;
+    private boolean removeMsg;
+    private boolean modifyMsg;
+    private boolean newMessage;
   
     /**
-     * Konstruktor zu Erstellung eines Benutzers
-     * @param benutzername
-     * @param abtNr 
+     * Konstruktor zum Erstellung des Benutzers
+     * @param user
+     * @param abtNr
+     * @param administrator
+     * @param message 
      */
-    public Client(String benutzername,int abtNr)
+    public Client(String user,int abtNr,boolean administrator,String message)
     {
-        this.benutzername = benutzername;
+        this.benutzerName = benutzerName;
         this.abtNr = abtNr;
+        this.administrator=administrator;
+        this.message =message;
     }
     
     public String getbenutzername()
     {
-        return benutzername;
+        return benutzerName;
     }
-   
+    
     public int getabtNr()
     {
         return abtNr;
@@ -57,9 +70,29 @@ public class Client {
     {
         return ip;
     }
-    
+
+    public boolean sendeMessage()
+    {
+        
+    }
     static public int hauptschleife()
     {
+        while(true)
+        {
+            
+           /* int auswahl=0;
+            switch(auswahl)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                
+            }*/
+            break;    
+        }
         return ENDE;
     }
     
@@ -67,9 +100,8 @@ public class Client {
     
     public static void main(String[] args) {
         // TODO code application logic here
-        int a; //tolle variable
         int ende ;
         ende = hauptschleife();
-        System.out.println("hi");
+        System.out.println("Client geschlossen");
     }
 }
