2db2d82838925f9772468e35dcc76d322d4547ab
fschjg263 <alexander.mueller187@gmail.com>
2016-06-03 10:21:41 +0200

    Variablen++

--- VerteilteAnzeigetafel/Anzeigetafel.java
+++ VerteilteAnzeigetafel/Anzeigetafel.java
@@ -1,5 +1,13 @@
 package VerteilteAnzeigetafel;
 
-public class Anzeigetafel {
+import java.util.AbstractSet;
+import java.util.ArrayList;
 
+public class Anzeigetafel {
+    private int abteilungsID;
+    private int messageAnzahl;
+    private int koordinatorID;
+    private ArrayList localMessages;
+    private ArrayList globalMessages;
+    private AbstractSet userIDs;
 }
