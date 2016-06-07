651c0ba0388eb8d24d34f622427b78bcab9c30e2
ki-am <alexander_mueller1987@web.de>
2016-06-07 15:03:20 +0200

    anzeigetafel und message erweitert

--- VerteilteAnzeigetafel/Anzeigetafel.java
+++ VerteilteAnzeigetafel/Anzeigetafel.java
@@ -1,36 +1,85 @@
 package VerteilteAnzeigetafel;
 //vsemenishch, Git funktioniert!
-import java.util.AbstractSet;
+
 import java.util.ArrayList;
+import java.util.TreeSet;
 
 public class Anzeigetafel {
+    private static String lastID;
     private int abteilungsID;
     private int messageAnzahl;
     private int koordinatorID;
     private ArrayList localMessages;
     private ArrayList globalMessages;
-    private AbstractSet userIDs;
+    private TreeSet userIDs;
     
-    public boolean isUser(){
-        return false;
+    public Anzeigetafel(){
+        /* welche Nummern sollen die einzelnen Anzeigetafeln bekommen?*/
+        this.abteilungsID=1; // hier muss man fuer jedes kompilierte Programm
+                                // eine neue Nummer festlegen
+        this.koordinatorID=1;
+        this.messageAnzahl=0;
+        Anzeigetafel.lastID = null;
+        this.localMessages = new ArrayList<>();
+        this.globalMessages = new ArrayList<>();
+        this.userIDs = new TreeSet<>();
+        /* Füge 5 users ein die zu dieser Anzeigetafel gehören*/
+        for(int i = 1; i == 5;i++){
+            userIDs.add(i);
+        }
     }
     
-    public boolean isCoordinator(){
-        return false;
+    
+    
+    public boolean isUser(int userID) {
+        return userIDs.contains(userID);
+    }
+
+    public boolean isCoordinator(int userID) {
+        return userID == koordinatorID;
+    }
+
+    public void modifyMessage(int messageID, String inhalt) {
+        for (int i=0;i<localMessages.size();i++){
+            Message msg = (Message)localMessages.get(i);
+            if(msg.getMessageID()==messageID){
+                //TODO code zum Modifizieren
+            }
+        }
+    }
+
+    public void deleteMessage(int messageID) {
+        /* Entferne die Message wenn vorhanden aus localMessages 
+            und globalMessages */
+        for (int i=0;i<localMessages.size();i++){
+            Message msg = (Message)localMessages.get(i);
+            if(msg.getMessageID()==messageID){
+                localMessages.remove(i);
+            }
+        }
+        for (int i=0;i<globalMessages.size();i++){
+            Message msg = (Message)globalMessages.get(i);
+            if(msg.getMessageID()==messageID){
+                globalMessages.remove(i);
+            }
+        }
     }
     
-    public void modifyMessage(int messageID, String inhalt){
-        
+    public int createMessage(Message msg) {
+        int msgID = Integer.parseInt(getNewMsgID(msg.getUserID()));
+        Message nMsg = new Message(msg,msgID);
+        localMessages.add(nMsg);
+        if(nMsg.isOeffentlich()){
+            globalMessages.add(nMsg);
+        }
+        return msgID;
     }
     
-    public void deleteMessage(int messageID){
-        
+    private String getNewMsgID(int userID){
+        Anzeigetafel.lastID = ""+userID+abteilungsID+System.currentTimeMillis()%1000;
+        return Anzeigetafel.lastID;
     }
-    
-    public void createMessage(){
-        
-    }
-    
+
     public int getAbteilungsID() {
         return abteilungsID;
     }
@@ -51,8 +100,8 @@ public class Anzeigetafel {
         return globalMessages;
     }
 
-    public AbstractSet getUserIDs() {
+    public TreeSet getUserIDs() {
         return userIDs;
     }
-    
+
 }
