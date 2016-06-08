e54b4d81936615c8b1a9cdb893830b51e7ac7482
ki-am <alexander_mueller1987@web.de>
2016-06-08 23:21:56 +0200

    ArrayList auf HashMap geaendert um besser mit MessageIDs arbeiten zu koennen + kleine Aenderungen

--- VerteilteAnzeigetafel/Anzeigetafel.java
+++ VerteilteAnzeigetafel/Anzeigetafel.java
@@ -2,35 +2,35 @@ package VerteilteAnzeigetafel;
 //vsemenishch, Git funktioniert!
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.TreeSet;
 
 public class Anzeigetafel {
+
     private static String lastID;
-    private int abteilungsID;
+    private final int abteilungsID;
     private int messageAnzahl;
-    private int koordinatorID;
-    private ArrayList localMessages;
-    private ArrayList globalMessages;
+    private final int koordinatorID;
+    private HashMap localMessages;
+    private HashMap globalMessages;
     private TreeSet userIDs;
-    
-    public Anzeigetafel(){
+
+    public Anzeigetafel() {
         /* welche Nummern sollen die einzelnen Anzeigetafeln bekommen?*/
-        this.abteilungsID=1; // hier muss man fuer jedes kompilierte Programm
-                                // eine neue Nummer festlegen
-        this.koordinatorID=1;
-        this.messageAnzahl=0;
+        this.abteilungsID = 1; // hier muss man fuer jedes kompilierte Programm
+        // eine neue Nummer festlegen
+        this.koordinatorID = 1;
+        this.messageAnzahl = 0;
         Anzeigetafel.lastID = null;
-        this.localMessages = new ArrayList<>();
-        this.globalMessages = new ArrayList<>();
+        this.localMessages = new HashMap();
+        this.globalMessages = new HashMap();
         this.userIDs = new TreeSet<>();
-        /* Füge 5 users ein die zu dieser Anzeigetafel gehören*/
-        for(int i = 1; i == 5;i++){
+        /* Füge 5 users ein, die zu dieser Anzeigetafel gehören*/
+        for (int i = 1; i < 6; i++) {
             userIDs.add(i);
         }
     }
-    
-    
-    
+
     public boolean isUser(int userID) {
         return userIDs.contains(userID);
     }
@@ -40,43 +40,53 @@ public class Anzeigetafel {
     }
 
     public void modifyMessage(int messageID, String inhalt) {
-        for (int i=0;i<localMessages.size();i++){
-            Message msg = (Message)localMessages.get(i);
-            if(msg.getMessageID()==messageID){
-                //TODO code zum Modifizieren
+
+        if (localMessages.containsKey(messageID)){
+            Message msg = (Message)localMessages.get(messageID);
+            if(msg.getUserID()==koordinatorID /*|| msg.getUserID()==modifier*/){
+                Message m = new Message(new Message(inhalt,
+                        msg.getUserID(),msg.getAbtNr(),
+                        msg.isOeffentlich()),msg.getMessageID());
+                localMessages.replace(messageID, m);
+            }else {
+                // sollen Methoden Rückgabewerte für failures haben??
+                // bzw. Berechtigungen prüfen ??
+               // return 1;
             }
+            
         }
+
     }
 
     public void deleteMessage(int messageID) {
         /* Entferne die Message wenn vorhanden aus localMessages 
             und globalMessages */
-        for (int i=0;i<localMessages.size();i++){
-            Message msg = (Message)localMessages.get(i);
-            if(msg.getMessageID()==messageID){
-                localMessages.remove(i);
+        if(localMessages.containsKey(messageID)){
+            localMessages.remove(messageID);
+            if (globalMessages.containsKey(messageID)){
+                /* sollen öffentliche Nachrichten gleiche ID haben, wenn sie 
+                * auch in lokalen Nachrichten sind? 
+                */
+                globalMessages.remove(messageID);
             }
+            messageAnzahl--;
         }
-        for (int i=0;i<globalMessages.size();i++){
-            Message msg = (Message)globalMessages.get(i);
-            if(msg.getMessageID()==messageID){
-                globalMessages.remove(i);
-            }
-        }
+        
     }
-    
+
     public int createMessage(Message msg) {
         int msgID = Integer.parseInt(getNewMsgID(msg.getUserID()));
-        Message nMsg = new Message(msg,msgID);
-        localMessages.add(nMsg);
-        if(nMsg.isOeffentlich()){
-            globalMessages.add(nMsg);
+        Message nMsg = new Message(msg, msgID);
+        localMessages.put(nMsg.getMessageID(), nMsg);
+        if (nMsg.isOeffentlich()) {
+            globalMessages.put(nMsg.getMessageID(), nMsg);
         }
+        messageAnzahl++;
         return msgID;
     }
-    
-    private String getNewMsgID(int userID){
-        Anzeigetafel.lastID = ""+userID+abteilungsID+System.currentTimeMillis()%1000;
+
+    private String getNewMsgID(int userID) {
+        Anzeigetafel.lastID = "" + userID + abteilungsID + System.currentTimeMillis() % 1000;
         return Anzeigetafel.lastID;
     }
 
@@ -92,16 +102,20 @@ public class Anzeigetafel {
         return koordinatorID;
     }
 
-    public ArrayList getLocalMessages() {
+    public HashMap getLocalMessages() {
         return localMessages;
     }
 
-    public ArrayList getGlobalMessages() {
+    public HashMap getGlobalMessages() {
         return globalMessages;
     }
 
     public TreeSet getUserIDs() {
         return userIDs;
+    }
+
+    public static String getLastID() {
+        return lastID;
     }
 
 }
