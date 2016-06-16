7ca57e299383d1d4dda0b1c33ec6b2f3260ff738
ki-am <alexander_mueller1987@web.de>
2016-06-16 12:07:25 +0200

    getMessagesByUserID in Anzeigetafel

--- VerteilteAnzeigetafel/Anzeigetafel.java
+++ VerteilteAnzeigetafel/Anzeigetafel.java
@@ -15,6 +15,7 @@ public class Anzeigetafel implements Serializable {
     private final int koordinatorID;
     private HashMap<Integer, Message> Messages;
     private HashSet<Integer> userIDs;
+    private HashMap<Integer, LinkedList<Integer>> userMsgs;
 
     public Anzeigetafel() {
         /* welche Nummern sollen die einzelnen Anzeigetafeln bekommen?*/
@@ -79,13 +80,12 @@ public class Anzeigetafel implements Serializable {
     }
 
     public void deleteMessage(int messageID, int user) throws TafelException {
-        /* Entferne die Message wenn vorhanden aus localMessages 
-            und globalMessages */
-
+        
         if (Messages.containsKey(messageID)) {
             /* Autor oder Koordinator ?*/
             if (user == Messages.get(messageID).getUserID() || isCoordinator(user)) {
                 Messages.remove(messageID);
+                userMsgs.get(user).remove(messageID);
             } else {
                 throw new TafelException(user + " nicht berechtigt zum Löschen");
             }
@@ -102,6 +102,11 @@ public class Anzeigetafel implements Serializable {
         int msgID = Integer.parseInt(getNewMsgID(msg.getUserID()));
         Message nMsg = new Message(msg, msgID);
         Messages.put(nMsg.getMessageID(), nMsg);
+        /* noch kein user da*/
+        if(!userMsgs.containsKey(user)){
+            userMsgs.put(user, new LinkedList<Integer>());
+        }
+        userMsgs.get(user).add(msgID);
         messageAnzahl++;
         msgLaufNr++;
         return msgID;
@@ -184,5 +189,11 @@ public class Anzeigetafel implements Serializable {
         }
         return at;
     }
-
+    
+    public LinkedList<Integer> getMessagesByUserID(int userID) throws TafelException{
+        if(!userMsgs.containsKey(userID))
+            throw new TafelException("Kein User gefunden!");
+        return userMsgs.get(userID);
+    }
+    
 }
