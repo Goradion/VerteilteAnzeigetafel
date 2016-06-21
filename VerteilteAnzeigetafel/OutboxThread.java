b4846591cec984ded8f40e4776c68e556549b706
sbastian <sbastian@kil-m-06.stl.htwsaar.de>
2016-06-21 17:13:11 +0200

    new Socket for each request

--- VerteilteAnzeigetafel/OutboxThread.java
+++ VerteilteAnzeigetafel/OutboxThread.java
@@ -3,35 +3,48 @@ package VerteilteAnzeigetafel;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.net.SocketAddress;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class OutboxThread extends Thread {
 	int abteilungsID;
-	Socket socket;
+	SocketAddress adress;
 	LinkedBlockingQueue<Message> messageQueue;
 
-	public OutboxThread(int abteilungsID, Socket socket, LinkedBlockingQueue<Message> messageQueue) {
+	public OutboxThread(int abteilungsID, SocketAddress adress, LinkedBlockingQueue<Message> messageQueue) {
 		super();
 		this.abteilungsID = abteilungsID;
-		this.socket = socket;
+		this.adress = adress;
 		this.messageQueue = messageQueue;
 	}
 
 	public void run() {
-
+		Socket socket = null; 
 		try {
 			while (true) {
+				socket = new Socket();
+				socket.connect(adress);
 				ObjectOutputStream oout = new ObjectOutputStream(socket.getOutputStream());
 				Message msg = messageQueue.take();
-				ServerRequest request = ServerRequest.buildCreateRequest(msg.getInhalt(), msg.getUserID(), 
+				ServerRequest request = ServerRequest.buildCreateRequest(msg.getInhalt(), msg.getUserID(),
 						msg.getUserID(), msg.isOeffentlich());
 				oout.writeObject(request);
+				
 			}
 		} catch (InterruptedException e) {
-			TafelServer.print("OutboxThread für Abteilung "+abteilungsID+" wurfe unterbrochen!");
+			TafelServer.print("OutboxThread für Abteilung " + abteilungsID + " wurde unterbrochen!");
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+			try {
+				if (socket != null && !socket.isClosed()) {
+					socket.close();
+				}
+			} catch (IOException e) {
+				System.err.println(
+						"Socket in OutboxThread der Abteilung " + abteilungsID + " konnte nicht geschlossen werden!");
+				e.printStackTrace();
+			}
 		}
 
 	}
