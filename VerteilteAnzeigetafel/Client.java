55588f78c7a6ca22b41a6be7374f7cc09df5cffa
sbastian <sbastian@kil-m-07.stl.htwsaar.de>
2016-06-21 12:33:45 +0200

    more testing

--- VerteilteAnzeigetafel/Client.java
+++ VerteilteAnzeigetafel/Client.java
@@ -21,29 +21,29 @@ public class Client {
 	public static void main(String[] args) {
 		try {
 			Socket socket = new Socket(SERVER_HOSTNAME, SERVER_PORT);
-			ServerRequest sr = new ServerRequest(ServerRequestType.CREATE, 0, "hi", 0, 1);
-			// ServerRequest sr = new
-			// ServerRequest(ServerRequestType.DELETE,31792,"asdf",3,1);
+			ServerRequest sr = ServerRequest.buildShowMyMessagesRequest(1);
+			//ServerRequest sr = ServerRequest.buildCreateRequest("test", 1, 1);
 			ObjectOutputStream oout = new ObjectOutputStream(socket.getOutputStream());
 			// sr =
 			// ServerRequest.buildShowMyMessagesRequest(ServerRequestType.SHOW_MY_MESSAGES,
 			// 1);
 			oout.writeObject(sr);
-			byte[] b = new byte [128];
-			InputStream stream = socket.getInputStream();
-			while (stream.available() == 0);
-			stream.read (b);
-			System.out.println(new String(b));
-			// ObjectInputStream input = new
-			// ObjectInputStream(socket.getInputStream());
-			// LinkedList<Message> userMessages =(LinkedList<Message>)
-			// input.readObject();
-			// System.out.println(userMessages.toString());
+			// byte[] b = new byte [128];
+			// InputStream stream = socket.getInputStream();
+			// while (stream.available() == 0);
+			// stream.read (b);
+			// System.out.println(new String(b));
+			ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+			LinkedList<Message> userMessages = (LinkedList<Message>) input.readObject();
+			System.out.println(userMessages.toString());
 			socket.close();
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
