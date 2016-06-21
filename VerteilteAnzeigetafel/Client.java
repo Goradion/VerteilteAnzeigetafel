b0ad839bbb96f17f0577f82d1ccc4561b985bec8
sbastian <sbastian@kil-m-06.stl.htwsaar.de>
2016-06-21 17:15:13 +0200

    testing stuff

--- VerteilteAnzeigetafel/Client.java
+++ VerteilteAnzeigetafel/Client.java
@@ -21,8 +21,8 @@ public class Client {
 	public static void main(String[] args) {
 		try {
 			Socket socket = new Socket(SERVER_HOSTNAME, SERVER_PORT);
-			ServerRequest sr = ServerRequest.buildShowMyMessagesRequest(1);
-			//ServerRequest sr = ServerRequest.buildCreateRequest("test", 1, 1);
+			//ServerRequest sr = ServerRequest.buildShowMyMessagesRequest(1);
+			ServerRequest sr = ServerRequest.buildCreateRequest("test", 1, 1,false);
 			ObjectOutputStream oout = new ObjectOutputStream(socket.getOutputStream());
 			// sr =
 			// ServerRequest.buildShowMyMessagesRequest(ServerRequestType.SHOW_MY_MESSAGES,
@@ -33,9 +33,9 @@ public class Client {
 			// while (stream.available() == 0);
 			// stream.read (b);
 			// System.out.println(new String(b));
-			ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
-			LinkedList<Message> userMessages = (LinkedList<Message>) input.readObject();
-			System.out.println(userMessages.toString());
+//			ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+//			LinkedList<Message> userMessages = (LinkedList<Message>) input.readObject();
+//			System.out.println(userMessages.toString());
 			socket.close();
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
@@ -43,9 +43,10 @@ public class Client {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
+//		 catch (ClassNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	}
 }
